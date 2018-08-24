4ec92090392c8154c2b21a88ff95b26111ae1370
Austin <austin.cotant@gmail.com>
2018-08-23 22:49:49 -0700

    Add methods to map #key? #set

--- w1d5/map.rb
+++ w1d5/map.rb
@@ -5,7 +5,8 @@ class Map
   end
 
   def set(key, value)
-    
+    key_index = @m.key?(key)
+    key_index ? @m[key_index] = [key, value] : @m << [key, value]
   end
 
   def get(key)
@@ -18,6 +19,15 @@ class Map
 
   def show
 
+  end
+
+  private
+
+  def key?(target)
+    @m.each_with_index do |key_value_pair, index|
+      return index if key_value_pair[0] == target
+    end
+    false
   end
 
 end
