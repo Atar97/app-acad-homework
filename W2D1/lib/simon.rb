03b8022ae2b68524abe11ed3ecd42c8e9cc44512
Austin <austin.cotant@gmail.com>
2018-08-26 21:48:25 -0700

    Add lots of features, fully functioning color game

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -1,5 +1,6 @@
 require 'byebug'
 require 'colorize'
+require 'colorized_string'
 
 class Simon
   COLORS = %w(red blue green yellow)
@@ -10,14 +11,45 @@ class Simon
     @sequence_length = 1
     @game_over = false
     @seq = []
+    @challenge = false
   end
 
   def play
+    start_game
     until @game_over
       take_turn
     end
     game_over_message
     reset_game
+  end
+
+  def start_game
+    system('clear')
+    print_instructions
+    print "Would you like to try challenge mode (y/n)?"
+    @challenge = gets.chomp == 'y'
+    puts "Memorize the word, not the color!" if @challenge
+    puts "Ready".colorize(:red)
+    sleep(0.5)
+    puts "Set".colorize(:yellow)
+    sleep(0.5)
+    puts "GO!!!!".colorize(:green)
+    sleep(1)
+    system("clear")
+    sleep(1)
+  end
+
+  def print_instructions
+    puts "Welcome the Simon Memory Game".colorize(:orange)
+    colored_colors = COLORS.map do |color|
+      color.colorize(color.to_sym)
+    end
+    puts "Colors will appear on the screen and you must memorize the sequence " \
+    "then repeat it back to the computer."
+    print "The colors are : "
+    colored_colors.each {|el| print el + " "}
+    sleep(6)
+    system('clear')
   end
 
   def take_turn
@@ -32,14 +64,30 @@ class Simon
   def show_sequence
     add_random_color
     @seq.each do |color|
-      puts color.color
+      print "Color: ".colorize(:white)
+      sleep(0.5)
+      if @challenge
+        puts color.colorize(COLORS.sample.to_sym)
+      else
+        puts color.colorize(color.to_sym)
+      end
       sleep(1)
       system('clear')
     end
   end
 
   def require_sequence
-
+    print "Enter color 1: "
+    for i in (0...@sequence_length)
+      input = gets.chomp
+      if input == @seq[i]
+        puts "Nice!"
+        print "Next color: "
+      else
+        @game_over = true
+        return
+      end
+    end
   end
 
   def add_random_color
@@ -47,11 +95,14 @@ class Simon
   end
 
   def round_success_message
+    puts "You completed round #{@sequence_length}!"
+    sleep(2)
+    system("clear")
 
   end
 
   def game_over_message
-
+    puts "YOU FAILED".colorize(:red)
   end
 
   def reset_game
@@ -60,3 +111,6 @@ class Simon
     @seq = []
   end
 end
+
+game = Simon.new
+game.play
