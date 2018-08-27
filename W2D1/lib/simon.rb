01279da6be00cb4eb28d0af197b6a6e8d546dab1
Austin <austin.cotant@gmail.com>
2018-08-26 21:07:29 -0700

    All specs passed, building game

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -1,22 +1,41 @@
+require 'byebug'
+require 'colorize'
+
 class Simon
   COLORS = %w(red blue green yellow)
 
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless @game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts color.color
+      sleep(1)
+      system('clear')
+    end
   end
 
   def require_sequence
@@ -24,7 +43,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
@@ -36,6 +55,8 @@ class Simon
   end
 
   def reset_game
-
+    @game_over = false
+    @sequence_length = 1
+    @seq = []
   end
 end
